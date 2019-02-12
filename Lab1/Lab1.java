2256be7a811cda87a2d28251e498f3bc19ca95b0
Jiarong Chen <37850306+JRRRRRRR@users.noreply.github.com>
2019-02-11 23:00:49 -0500

    Add files via upload

---
+++ Lab1/Lab1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // for user input from keyboard
+public class Lab1 {
+	public static void main(String[] args){
+		double radius;//Declare radius
+		double area;//Declare area
+		System.out.println("Enter the radius:-->");
+		Scanner stdin = new Scanner(System.in);
+		radius = stdin.nextDouble();//New value is radius.
+		//Compute area
+		area = radius * radius * 3.14159;
+		//Display results
+		System.out.println("The area for the circle" + " of radius " + radius + " is " + area);
+		stdin.close();
+
+	}
+
+}
